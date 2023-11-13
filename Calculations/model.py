2b67d7db03a5aabb42c9bf681d848d3c65b5119d
Creeprus <isip_l.k.kornienko>
2023-11-13 20:45:14 +0300

    Additional hierarchy

--- Calculations/model.py
+++ Calculations/model.py
@@ -1,10 +1,19 @@
 import numpy as np
 import pandas as pd
+import requests
+import json
 from sklearn.model_selection import KFold
 from sklearn import metrics
 
 class Model:
+
+    def connect_to_api(self):
+        response_API = requests.get('https://api.covid19india.org/state_district_wise.json')
+        return response_API
+
     def read_csv(self):
-        pass
+        df = pd.read_csv("vgsales_5_for_machine.csv")
+        df
+
     def init_model(self):
         pass
