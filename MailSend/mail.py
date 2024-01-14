b361377fd69672b9692c0ed1b77831a2a3ba57a9
Creeprus <leva.kornienko@yandex.ru>
2024-01-14 19:39:19 +0300

    Rabbit sender

--- MailSend/mail.py
+++ MailSend/mail.py
@@ -4,11 +4,25 @@ from email.mime.multipart import MIMEMultipart
 from Prognosis.prognose_income import Prognose
 from Prognosis.prognose_income_reverse import Prognose_Reverse
 import strings
+from RabbitReader.rabbit_reader import RabbitReader
 
 
 class MailSender:
     def __init__(self, receiver):
         self.receiver = receiver
+
+    def send_metrics(self):
+        pass
+
+    def send_report_classic_rabbit(self, year, area, plant, prolificy_model, stock_price=170,
+                                   planting_price=39100):
+        rabbit = RabbitReader()
+        prognosis = Prognose(prolificy=prolificy_model, planting_price=planting_price, stock_price=stock_price,
+                             plant=plant)
+        income = prognosis.prognose_income()
+        profit = prognosis.prognose_profit()
+        body = f'{"plant":"{plant}", "area":{area}, "income":"{income}","profit":"{profit}"}'
+        rabbit.send_message(body=body, exchange=strings.rabbit_exchange, routing_key=strings.rabbit_classic_queue)
 
     def send_report_classic(self, year, area, plant, prolificy_model, stock_price=170,
                             planting_price=39100):
@@ -49,6 +63,33 @@ class MailSender:
             server.sendmail(
                 strings.smtp_mail, self.receiver, message.as_string()
             )
+
+    def send_report_reverse_rabbit(self, year, area, desired_profit, best_plants, stock_planting_price):
+        rabbit = RabbitReader()
+        prognosis = Prognose()
+        prognosis_reverse_first = Prognose_Reverse()
+        prognosis_reverse_second = Prognose_Reverse()
+        list_keys = prognosis.return_cultures(best_plants)
+        list_values = prognosis.return_prolificies(best_plants)
+        prognosis_reverse_first.plant, prognosis_reverse_second.plant = list_keys[0], list_keys[1]
+        prognosis_reverse_first.prolificy, prognosis_reverse_second.prolificy = list_values[0], list_values[1]
+        prognosis_reverse_first.stock_price, prognosis_reverse_second.stock_price = prognosis_reverse_first.return_prices(
+            stock_planting_price, f"{list_keys[0]} stock price"), prognosis_reverse_second.return_prices(
+            stock_planting_price,
+            f"{list_keys[1]} stock price")
+        prognosis_reverse_first.planting_price, prognosis_reverse_second.planting_price = prognosis_reverse_first.return_prices(
+            stock_planting_price, f"{list_keys[0]} planting price"), prognosis_reverse_second.return_prices(
+            stock_planting_price,
+            f"{list_keys[1]} planting price")
+        body = {
+            f"{list_keys[0]}": list_values[0],
+            f"{list_keys[1]}": list_values[1],
+            f"{list_keys[0]} income": prognosis_reverse_first.prognose_income(),
+            f"{list_keys[0]} profit": prognosis_reverse_first.prognose_profit(),
+            f"{list_keys[1]} income": prognosis_reverse_second.prognose_income(),
+            f"{list_keys[1]} profit": prognosis_reverse_second.prognose_income()
+        }
+        rabbit.send_message(body=body, exchange=strings.rabbit_exchange, routing_key=strings.rabbit_reverse_queue)
 
     def send_report_reverse(self, year, area, desired_profit, best_plants, stock_planting_price
                             ):
@@ -140,3 +181,10 @@ class MailSender:
             server.sendmail(
                 strings.smtp_mail, self.receiver, message.as_string()
             )
+
+    # def send_report_fail_rabbit(self):
+    #     rabbit = RabbitReader()
+    #     body = {
+    #         "fail": "Prognosis failed. Not enough data"
+    #     }
+    #     rabbit.send_message(body=body, exchange=strings.rabbit_exchange, routing_key=strings.rabbit_reverse_queue)
--- RabbitReader/rabbit_reader.py
+++ RabbitReader/rabbit_reader.py
@@ -27,10 +27,12 @@ class RabbitReader(Model):
             mail.send_report_fail()
             return
         result = self.init_model(df, year)
-        mail.send_report_classic(area=area, plant=plant, year=year,
-                                 prolificy_model=result, stock_price=stock_price, planting_price=planting_price)
+        # mail.send_report_classic(area=area, plant=plant, year=year,
+        #                          prolificy_model=result, stock_price=stock_price, planting_price=planting_price)
+        mail.send_report_classic_rabbit(area=area, plant=plant, year=year,
+                                        prolificy_model=result, stock_price=stock_price, planting_price=planting_price)
 
-    def receive_message(self):
+    def create_channel(self):
         credentials = pika.PlainCredentials(strings.rabbit_user, strings.rabbit_pass)
         connection = pika.BlockingConnection(
             pika.ConnectionParameters(host=strings.rabbit_host, credentials=credentials))
@@ -41,8 +43,18 @@ class RabbitReader(Model):
         channel.queue_declare(queue=strings.rabbit_reverse_queue, durable=True)
         channel.basic_consume(queue=strings.rabbit_reverse_queue, on_message_callback=self.reverse_report,
                               auto_ack=True)
+        return channel
+
+    def receive_message(self):
+        channel = self.create_channel()
         print(' [*] Waiting for report queue')
         channel.start_consuming()
+
+    def send_message(self, body, routing_key, exchange):
+        channel = self.create_channel()
+        channel.basic_publish(exchange=exchange,
+                              routing_key=routing_key,
+                              body=body)
 
     def reverse_report(self, cg, method, properties, body):
         print(f" [x] Received {body}")
@@ -65,5 +77,7 @@ class RabbitReader(Model):
             planting_price = self.get_planting_price(df, area, key, year)
             plant_stock_price.update({f"{key} stock price": stock_price})
             plant_stock_price.update({f"{key} planting price": planting_price})
-        mail.send_report_reverse(area=area, best_plants=result_plants, year=year,
-                                 desired_profit=desired_profit, stock_planting_price=plant_stock_price)
+        # mail.send_report_reverse(area=area, best_plants=result_plants, year=year,
+        #                          desired_profit=desired_profit, stock_planting_price=plant_stock_price)
+        mail.send_report_reverse_rabbit(area=area, best_plants=result_plants, year=year,
+                                        desired_profit=desired_profit, stock_planting_price=plant_stock_price)
